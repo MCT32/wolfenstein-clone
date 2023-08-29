4aca2cc982b0660031856b5941df9849c4487626
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-08-29 18:52:37 +1000

    window made

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let _window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("Close requested, exiting...");
+                control_flow.set_exit();
+            },
+            _ => ()
+        }
+    }).unwrap();
 }
