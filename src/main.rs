8bf5b64412db7e34d5194451b8b1a19536ea5a7f
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-08-29 19:15:15 +1000

    pixel rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::num::NonZeroU32;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
@@ -6,7 +7,10 @@ use winit::{
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let _window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
@@ -18,6 +22,31 @@ fn main() {
             } => {
                 println!("Close requested, exiting...");
                 control_flow.set_exit();
+            },
+            Event::RedrawRequested(window_id) => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+                
+                let mut buffer = surface.buffer_mut().unwrap();
+                for index in 0..(width * height) {
+                    let y = index / width;
+                    let x = index % width;
+                    let red = x % 255;
+                    let green = y % 255;
+                    let blue = (x * y) % 255;
+
+                    buffer[index as usize] = blue | (green << 8) | (red << 16);
+                }
+
+                buffer.present().unwrap();
             },
             _ => ()
         }
