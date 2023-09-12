eea90e0d734921bd366039c6f5300c161ec0457a
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-09-12 22:11:27 +1000

    render shading

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,6 @@
+use std::ops::{ Add, Mul };
+use std::fmt;
+
 pub struct Color {
     red: u8,
     green: u8,
@@ -11,5 +14,40 @@ impl Color {
 
     pub fn to_u32(&self) -> u32 {
         self.blue as u32 | (self.green as u32) << 8 | (self.red as u32) << 16
+    }
+
+    pub fn interp(v1: Color, v2: Color, fac: f32) -> Color {
+        let fac = fac.clamp(0.0, 1.0);
+        v1 * (1.0 - fac) + v2 * fac
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            red: (self.red + other.red).clamp(0, 255),
+            green: (self.green + other.red).clamp(0, 255),
+            blue: (self.blue + other.blue).clamp(0, 255),
+        }
+    }
+}
+
+impl Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            red: (self.red as f32 * rhs) as u8,
+            green: (self.green as f32 * rhs) as u8,
+            blue: (self.blue as f32 * rhs) as u8,
+        }
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.red, self.green, self.blue)
     }
 }
--- src/render.rs
+++ src/render.rs
@@ -18,7 +18,8 @@ pub fn render(map: &map::Map, player: &player::Player, buffer: &mut softbuffer::
             let height = std::cmp::min((height * HEIGHT as f32) as u32, HEIGHT);
 
             for j in 0..height {
-                buffer[(i + (j + (HEIGHT - height) / 2) * WIDTH) as usize] = color::Color::new(0, 255, 0).to_u32();
+                let pixel = color::Color::interp(color::Color::new(0, 255, 0), color::Color::new(255, 255, 255), distance / 5.0);
+                buffer[(i + (j + (HEIGHT - height) / 2) * WIDTH) as usize] = pixel.to_u32();
             }
         }
     }
